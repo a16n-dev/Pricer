3a00e42c688f5b0abd5b0accaf63c7e5148d0fcb
Alexander Nicholson <alexn400@gmail.com>
2020-12-17 22:37:08 +1300

    Setup react app with typescript and bootstrap

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button color='primary'>This is a bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
