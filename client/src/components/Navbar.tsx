c087e7e1e696bd9bc54a96dba00a4885c2cafcb5
Alexander Nicholson <alexn400@gmail.com>
2021-01-02 19:37:36 +1300

    update nav to use history hook over href

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Button, Nav, NavItem, NavLink } from 'reactstrap';
 import { logout } from '../redux/AuthSlice';
 
 const Navbar: React.FC = () => {
 
   const dispatch = useDispatch();
-  
+  const history = useHistory();
+
+  const navItems = [
+    {link: '/', text: 'Dashboard'},
+    {link: '/recipes', text: 'Recipes'},
+    {link: '/products', text: 'Products'},
+    {link: '/units', text: 'Units'},
+  ];
+
   return(
     <>
       <Nav className={'my-3'}>
         <NavItem className={'mr-4'}>
           <h4>Pricer</h4>
         </NavItem>
-        <NavItem>
-          <NavLink href="/">Dashboard</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/recipes">Recipes</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/products">Products</NavLink>
-        </NavItem>
+        {navItems.map((v, i) => (
+          <NavItem>
+            <Button color={'link'} onClick={() => history.push(v.link)}>{v.text}</Button>
+          </NavItem>
+        ))}
         <NavItem className={'ml-auto'}>
           <Button outline color={'primary'} onClick={() => {
             dispatch(logout());
